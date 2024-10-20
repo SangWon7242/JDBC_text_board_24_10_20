a768a4937c3f0cde19da1c29eb85c836ca27dc48
SangWon7242 <gotkdqja@gmail.com>
2024-10-20 17:51:51 +0900

    기본 명령어 기반 마련

--- src/main/java/com/sbs/jdbc/board/Main.java
+++ src/main/java/com/sbs/jdbc/board/Main.java
@@ -1,7 +1,51 @@
 package com.sbs.jdbc.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 자바 텍스트 게시판 시작 ==");
+
+    int lastArticleId = 0;
+
+    Scanner sc = new Scanner(System.in);
+
+    while(true) {
+      System.out.print("명령) ");
+      String cmd = sc.nextLine();
+
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성 ==");
+
+        System.out.print("제목 : ");
+        String subject = sc.nextLine();
+
+        if(subject.trim().isEmpty()) {
+          System.out.println("제목을 입력해주세요.");
+          continue;
+        }
+
+        System.out.print("내용 : ");
+        String content = sc.nextLine();
+
+        if(content.trim().isEmpty()) {
+          System.out.println("내용을 입력해주세요.");
+          continue;
+        }
+
+        int id = ++lastArticleId;
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      }
+      else if(cmd.equals("exit")) {
+        System.out.println("== 게시판을 종료합니다. ==");
+        break; // 이 시점에서 반복문을 빠져나옴
+      }
+      else {
+        System.out.println("잘못 입력 된 명령어입니다.");
+      }
+    }
+
+    sc.close(); // 메모리 반납
   }
 }
