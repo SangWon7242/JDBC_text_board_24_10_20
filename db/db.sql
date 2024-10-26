8d9a409161a7e2e15a229c5fc42b98c5276a1f2f
SangWon7242 <gotkdqja@gmail.com>
2024-10-26 10:45:00 +0900

    JDBCInsertTest 구현

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,12 @@
 DROP DATABASE IF EXISTS text_board;
 CREATE DATABASE text_board;
 USE text_board;
+
+# article 테이블 생성
+CREATE TABLE article (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	`subject` CHAR(100) NOT NULL,
+	content TEXT NOT NULL
+);
